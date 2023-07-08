71a8c1310b968d7c775950fff2fe098f417235d7
Katniss-Nataliia <natko.boiko@gmail.com>
2023-07-08 15:05:58 -0600

    added info

---
+++ src/index.js
@@ -0,0 +1,44 @@
+//API key and endpoint 
+
+const API_KEY = "6a391254f6cc39adc714e97cc9098241";
+const endpoint = "api.openweathermap.org";
+
+//Selecting DOM elements
+
+const form = document.querySelector(".search-form");
+const searchQuery = document.querySelector('input[name = "searchQuery"]');
+
+let currentSearchQuery = '';
+
+const gallery = document.querySelector("gallery")
+
+// add event listener for form submit
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    currentSearchQuery = searchQuery.value;
+    // gallery.innerHTML = '';
+    apiFetchWeather(currentSearchQuery) // fethc weather based on the search query;
+});
+
+
+
+//function to fetch weatehr data from Open Weatehr Map API
+
+
+function apiFetchWeather(query){
+    const weatherURL = `${endpoint}/data/2.5/weather?&units="metric"&q=${query}&appid=${API_KEY}`
+    axios
+    .get(weatherURL)
+    .then(function(response){
+        const data = response.data;
+        console.log(data)
+    })
+    .catch(error => console.log(error))
+
+}
+
+
+
+
+
