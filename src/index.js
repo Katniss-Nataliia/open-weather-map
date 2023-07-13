6ac1a9f116d608f00a8b246e64f00470c20d4574
Katniss-Nataliia <natko.boiko@gmail.com>
2023-07-12 22:18:34 -0600

    coded

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 //API key and endpoint 
-
 const API_KEY = "6a391254f6cc39adc714e97cc9098241";
 const endpoint = "https://api.openweathermap.org/data/2.5/weather";
 
 //Selecting DOM elements
-
 const form = document.querySelector(".search-form");
 const searchQuery = document.querySelector('input[name = "searchQuery"]');
+const gallery = document.querySelector('gallery');
 
 let currentSearchQuery = '';
-
-const gallery = document.querySelector("gallery")
+let lang = 'en';
+let units = 'metric'
 
 // add event listener for form submit
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
     currentSearchQuery = searchQuery.value;
-    // gallery.innerHTML = '';
-    apiFetchWeather(currentSearchQuery) // fethc weather based on the search query;
+    fetchWeather(currentSearchQuery) // fethc weather based on the search query;
 });
 
 
@@ -26,27 +24,42 @@ form.addEventListener('submit', function(event){
 //function to fetch weatehr data from Open Weatehr Map API
 
 
-function apiFetchWeather(query){
-    const weatherURL = `${endpoint}?units=metric&q=${query}&appid=${API_KEY}`
+function fetchWeather(query){
+    const weatherURL = `${endpoint}?units=${units}&q=${query}&appid=${API_KEY}&lang=${lang}`
     axios
     .get(weatherURL)
-    .then(function(response){
-        const data = response.data;
-        console.log(data)
+    .then((response) => {
+        // if(!response.ok) throw new Error(response.error);
+        // return response.json ()
+        const data = response.data; // why do we create a separate variable here?
+        console.log (data)
+        // showWeather(data);
+        const weatherMarkup = response.map(location =>
+            showWeather(location)).join('');
+        
+
+            updateWeather(weatherMarkup)
+    
     })
     .catch(error => console.log(error))
 
 }
 
 
-
 // function to render weather data in the gallery 
 
-function createMarkup (city){
-
+function showWeather(city){
+    // console.log(city);
     return `
-    
-    
-    `;
+    <div class='breed-card'>
+      
+      <p>${city.lat}</p>
+      <p ">${city.weather[0].main}</p></div>
+      <img >
+    </div>
+  `;
 }
 
+function updateWeather(markup){
+    gallery.innerHTML(markup)
+}
